2d739e96305127a8db3e9ef39e8b98adac0ff88d
Edson Angoti Júnior <angoti@gmail.com>
2022-06-06 21:47:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import ListagemLivros from "./components/ListagemLivros.js";
+import NovoLivro from "./components/NovoLivro.js";
+import EditaLivro from "./components/EditaLivro.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav className="container navbar navbar-expand navbar-dark bg-dark">
+        <h1 className="navbar-brand">Exemplo CRUD</h1>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/livros" className="nav-link">
+              Livros
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/novo" className="nav-link">
+              Novo livro
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<ListagemLivros />} />
+          <Route path="novo" element={<NovoLivro />} />
+          <Route path="livros/:id" element={<EditaLivro />} />
+          <Route path="livros" element={<ListagemLivros />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/services/Endpoints.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+export const getAll = () => {
+  return httpClient.get("/livro");
+};
+
+export const get = (id) => {
+  return httpClient.get(`/livro/${id}`);
+};
+
+export const create = (data) => {
+  return httpClient.post("/livro", data);
+};
+
+export const update = (id, data) => {
+  return httpClient.put(`/livro/${id}`, data);
+};
+
+export const remove = (id) => {
+  return httpClient.delete(`/livro/${id}`);
+};
